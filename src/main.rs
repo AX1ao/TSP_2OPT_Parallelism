dd23e4217cc2d5afec583fc999af6a028394f89a
Yuanyao Xiao <yx3493@crunchy1.cims.nyu.edu>
2025-04-11 14:49:55 -0400

    initialized main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+mod tsp;
+mod two_opt_seq;
+mod two_opt_par;
+
+use tsp::*;
+
 fn main() {
-    println!("Hello, world!");
+    let num_cities = 50;
+    let cities = generate_cities(num_cities);
+
+    // Random tour
+    let mut tour: Vec<usize> = (0..num_cities).collect();
+    shuffle_tour(&mut tour);
+
+    println!("Initial tour cost: {:.2}", compute_total_distance(&tour, &cities));
+
+    let start = std::time::Instant::now();
+    let (optimized_tour, final_cost) = two_opt_seq(&tour, &cities);
+    let duration = start.elapsed();
+
+    println!("Final tour cost: {:0.2}", final_cost);
+    println("Time taken: {:.2}", duration);
 }
